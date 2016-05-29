ea8b9a19e3e116ce728d04158baec8c6e31315ee
cstmdzx <cstmdzx@163.com>
2016-05-29 14:14:06 +0800

    first test

---
+++ GithubTest/GithubTest.cpp
@@ -0,0 +1,14 @@
+// GithubTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"Hello Github"<<endl;
+	return 0;
+}
+
